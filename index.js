2dbd90c5cb694d4df5d4651c8825f485af62ce93
Liam <liam.aharon@hotmail.com>
2017-03-19 00:20:05 +1100

    bona fide

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 var sessions = {};
+var user_session = {};
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/static/landing.html');
@@ -21,16 +22,26 @@ io.on('connection', function(socket){
     io.emit('lat long1', msg);
   });
 
-  socket.on('register', function(msg){
+  socket.on('disconnect', function(msg) {
+    console.log(user_session[socket.id]);
+    sessions[user_session[socket.id]] -= 1;
+    delete user_session[socket.id];
     console.log(sessions);
+  });
+
+  socket.on('register', function(msg){
+    user_session[socket.id] = msg;
+    console.log(user_session);
     if (sessions[msg] >= 2) {
-      io.emit('fuck off');
+      if (io.sockets.connected[socket.id]) {
+        io.sockets.connected[socket.id].emit('fuck off');
+      }
     } else if (sessions[msg]) {
       sessions[msg] += 1;
     } else {
       sessions[msg] = 1;
     }
-  	//console.log(user);
+    //console.log(sessions);
   })
 });
 
