60fbc314573e4563be65e1e0c3fa2750873ac3f1
Liam <liam.aharon@hotmail.com>
2017-03-18 22:46:13 +1100

    limit users per room

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
-var user = {};
+var sessions = {};
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/static/landing.html');
@@ -17,13 +17,17 @@ app.get('/connect/:id', function(req, res){
 
 io.on('connection', function(socket){
   socket.on('lat long1', function(msg){
-  	msg['clientId'] = socket.id;
-  	//console.log(msg);
     io.emit('lat long1', msg);
   });
 
   socket.on('register', function(msg){
-  	user[msg.username] = socket.id;
+    if (sessions.msg >= 2) {
+      window.location.replace("www.google.com");
+    } else if (sessions.msg) {
+       sessions.msg += 1;
+    } else {
+      sessions.msg = 1;
+    }
   	//console.log(user);
   })
 });
